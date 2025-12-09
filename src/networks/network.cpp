095189d9cd51cf948158abefe434482504939951
Christophe Favergeon <Christophe.Favergeon@arm.com>
2025-12-09 11:18:16 +0100

    Added networks

---
+++ src/networks/network.cpp
@@ -0,0 +1,26 @@
+extern "C" {
+#include "network.h"
+}
+
+namespace arm {
+namespace app {
+namespace kws {
+
+uint8_t *GetModelPointer();
+size_t GetModelLen();
+
+}  // namespace kws
+}  // namespace app
+}  
+
+
+const uint8_t * GetModelPointer()
+{
+   return arm::app::kws::GetModelPointer();
+}
+
+size_t GetModelLen()
+{
+   return arm::app::kws::GetModelLen();
+}
+
---
+++ src/networks/network.h
@@ -0,0 +1,10 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+const uint8_t * GetModelPointer();
+size_t GetModelLen();
+
+#endif
