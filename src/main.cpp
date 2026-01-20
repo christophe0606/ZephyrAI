5f55e9eaa5793a1a2a09f6f9d31bf37930fba461
Christophe Favergeon <Christophe.Favergeon@arm.com>
2026-01-20 11:30:26 +0100

    Changed main.cpp license header
    Original header was from a hello world example.
    The current file is a totally new implementation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
+/* ----------------------------------------------------------------------
+ * Project:      CMSIS Stream demo
+ * Title:        main.cpp
+ * Description:  Entry point for the CMSIS Stream demo application
+ *
+ * --------------------------------------------------------------------
+ *
+ * Copyright (C) 2026 ARM Limited or its affiliates. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <cstdio>
